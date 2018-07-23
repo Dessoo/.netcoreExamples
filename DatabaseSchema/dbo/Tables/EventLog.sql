506cfce9477f91769d439e3a2620e51190323cff
Desislav.Todorov <desoo.todorov@abv.bg>
2018-07-23 14:12:12 +0300

    add database schema

---
+++ DatabaseSchema/dbo/Tables/EventLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[EventLog] (
+    [Id]              BIGINT          IDENTITY (1, 1) NOT NULL,
+    [CreatedDateTime] DATETIME        NOT NULL,
+    [EventId]         INT             NULL,
+    [LogLevel]        NVARCHAR (50)   NOT NULL,
+    [Logger]          NVARCHAR (100)  NOT NULL,
+    [Message]         NVARCHAR (4000) NULL,
+    [Host]            NVARCHAR (100)  NOT NULL,
+    [Server]          NVARCHAR (100)  NULL
+);
+
---
+++ DatabaseSchema/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [id]        INT        IDENTITY (1, 1) NOT NULL,
+    [FirstName] NCHAR (50) NULL,
+    [LastName]  NCHAR (50) NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
